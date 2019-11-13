bd00ff36f0145155aed0680ddca44a904cddb19c
jungon <j017.kim@gmail.com>
2019-11-13 13:10:06 +0900

    modify exec-path on macos

--- init.el
+++ init.el
@@ -188,10 +188,10 @@
 ;;; Shell integration
 
 (use-package exec-path-from-shell
-  :if (memq window-system '(mac ns x))
+  :when (memq window-system '(mac ns x))
   :ensure t
   :config
-  (setq exec-path-from-shell-arguments nil)
+  (setq exec-path-from-shell-arguments '("-l"))
   (exec-path-from-shell-initialize)
   (exec-path-from-shell-copy-envs '("PATH")))
 
